ac0e64c7144da6331d67b7ae7fbbb55f6c25b522
optimabsc <201636790+optimabsc@users.noreply.github.com>
2025-09-14 21:53:05 +0200

    Create functions.php

---
+++ root/includes/functions.php
@@ -0,0 +1,25 @@
+<?php
+    //cleanse input data
+    function cleanse_input_data($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    //.env loader
+    function loadEnv($filePath) {
+        if (!file_exists($filePath)) {
+            throw new Exception("Environment file not found: $filePath");
+        }
+
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (str_starts_with(trim($line), '#')) continue; // Skip comments
+            [$key, $value] = explode('=', $line, 2);
+            $key = trim($key);
+            $value = trim($value);
+            $_ENV[$key] = $value;
+        }
+    }
+?>
